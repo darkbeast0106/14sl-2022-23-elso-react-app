0fd0d67be6eb66065e04d8814a9868be7e7da8d9
Budaházi Bence <budahazi.bence@petrik.hu>
2023-01-12 18:24:06 +0100

    blogbejegyzések felvétele és megjelenítése

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [bejegyzesek, setBejegyzesek] = useState([]);
+  const [cim, setCim] = useState("");
+  const [tartalom, setTartalom] = useState("");
+
+  const bejegyzesHozzaadasa = () => {
+    const bejegyzes = {
+      cim: cim,
+      tartalom: tartalom
+    };
+    const ujBejegyzesek = [];
+    bejegyzesek.forEach(bejegyzes => ujBejegyzesek.push(bejegyzes))
+    ujBejegyzesek.push(bejegyzes);
+    setBejegyzesek(ujBejegyzesek);
+  };
+
+  const bejegyzesLista = [];
+  bejegyzesek.forEach(bejegyzes => {
+    bejegyzesLista.push(<div>
+      <h4>{bejegyzes.cim}</h4>
+      <p>{bejegyzes.tartalom}</p>
+    </div>)
+  });
+
+  return (
+    <div>
+      <section>
+        <div>
+          <input
+            type="text"
+            placeholder="Cim"
+            value={cim}
+            onInput={(e) => {
+              setCim(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Tartalom"
+            value={tartalom}
+            onInput={e => setTartalom(e.target.value)}
+          ></textarea>
+        </div>
+        <button onClick={() => bejegyzesHozzaadasa()}>Felvesz</button>
+      </section>
+      <section>{ bejegyzesLista }</section>
+    </div>
+  );
+}
+
+export default App;
